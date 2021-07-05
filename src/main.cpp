caf438b0232ab94fbcbf64ade0a31c8f5ca4758f
Oleg <57217654+VelOBor@users.noreply.github.com>
2021-07-06 01:44:49 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 //
 //include all the necessary libraries
-#include <Arduino.h> //arduino library when using VSCode PlatformIO
+#include <Arduino.h> //arduino library when using VSCode PlatformIO, comment out or delete if using Arduino IDE
 #include <SPI.h> //SPI library for Arduino
 #include <RF24.h> //RF24 library for Arduino
 
